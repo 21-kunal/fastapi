c62db43a89fa1dadd84d7719abc3b37de25caa2d
21-kunal <kunalprajapati544@gmail.com>
2024-02-29 13:45:49 +0530

    created endpoints which supports the crud operarions

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
+
+my_posts = [{"title": "First Title","content": "First content","id": 1},{"title": "Second Title","content": "Second content","id":2}]
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = False
+    rating: int | None = None
+
+
+def find_post(id: int) -> dict|None:
+    for post in my_posts:
+        if post['id'] == id:
+            return post
+    return None
 
 @app.get("/")
 async def root():
     return {"msg": "Hello!!"}
 
+
 @app.get("/posts")
 async def get_posts():
-    return {"data": "This is a post"}
+    return {"data": my_posts}
 
-@app.post("/createPost")
-async def createPost(payLoad: dict = Body(...)):
-    print(payLoad)
-    return {"msg": payLoad}
+
+@app.post("/posts")
+async def createPost(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(1,1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/post/{id}")
+async def get_post(id: int):
+    return {"data": find_post(id)}
