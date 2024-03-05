d8dd497ba6fcdd2c216fa26c6c077ed13192d0ad
21-kunal <kunalprajapati544@gmail.com>
2024-03-05 20:56:55 +0530

    working with ORM, and using sqlAlchemy

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
 from pydantic import BaseModel
-from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
 from sqlalchemy.orm import Session
+
+
 from . import models
+from . import schemas
 from .database import engine,get_db
 
 models.Base.metadata.create_all(bind=engine)
@@ -29,12 +30,6 @@ while True:
 
 
 
-class Post(BaseModel):
-    title: str
-    content: str
-    published: bool = False
-
-
 @app.get("/")
 async def root():
     return {"msg": "Hello!!"}
@@ -42,24 +37,29 @@ async def root():
 
 @app.get("/sqlalchemy")
 def test_posts(db: Session = Depends(get_db)):
-    print(db)
-    return {'success'}
+    posts = db.query(models.Post).all()
+    return posts
 
 
 @app.get("/posts")
-async def get_posts():
-    cursor.execute("SELECT * FROM posts;")
-    posts = cursor.fetchall()
+async def get_posts(db: Session = Depends(get_db)):
+    # cursor.execute("SELECT * FROM posts;")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-async def createPost(post: Post):
-    cursor.execute("INSERT INTO posts ( title, content, published ) VALUES ( %s, %s, %s) RETURNING *;"
-                   ,(post.title,post.content,post.published))
-    new_post = cursor.fetchone()
+async def createPost(post: schemas.Post, db: Session = Depends(get_db)):
+    # cursor.execute("INSERT INTO posts ( title, content, published ) VALUES ( %s, %s, %s) RETURNING *;"
+    #                ,(post.title,post.content,post.published))
+    # new_post = cursor.fetchone()
     
-    conn.commit()
+    # conn.commit()
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data ": new_post}
 
 
@@ -89,7 +89,7 @@ async def delete_post(id: int):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @app.put("/posts/{id}")
-async def update_post(id: int, post: Post):
+async def update_post(id: int, post: schemas.Post):
 
     cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s 
                 WHERE id = %s RETURNING *;""",
